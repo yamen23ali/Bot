8b37b50ee10ac04bee0771c98bd1db4908f0d511
Yamen Ali <yamen.ali@zalando-lounge.de>
2016-12-27 22:36:41 +0100

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ require 'facebook/messenger'
 class User < ActiveRecord::Base
 	include Facebook::Messenger
 
-    def send_welcome_message(messenger_id)
+    def self.send_welcome_message(messenger_id)
     	message = 'السلام عليكم :) مرحبا بك في أول تطبيق إسلامي من نوعه على الفيسبوك سنقوم بإرسال آيات و أحاديث شريفة يوميا إليك .. نرجو لك الفائدة و الأجر'
         send_message(message, messenger_id)
     end
 
-    def send_message(message, messenger_id)
+    def self.send_message(message, messenger_id)
 		Bot.deliver({
 				recipient: {
 					id: messenger_id
